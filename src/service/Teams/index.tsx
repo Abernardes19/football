c05ac66d78e0388218b3f1bbf2ac4919c15a9462
Alexandre Bernardes <abernardes11192@gmail.com>
2023-05-23 16:42:20 -0300

    :sparkles: Adicionando requisição das estatisticas do time

--- src/service/Teams/index.tsx
+++ src/service/Teams/index.tsx
@@ -1,9 +1,9 @@
 import api from "../Api/api";
 import Cookies from "js-cookie";
-import { ITeamsRequest } from "./teams.structure";
+import { IStatiscsRequest, ITeamsRequest } from "./teams.structure";
 
 class Teams {
-  async getTeamsByLeagueAndSeason(league: string, season: number): Promise<ITeamsRequest[] | false> {
+  async getTeamsByLeagueAndSeason(league: string, season: string): Promise<ITeamsRequest[] | false> {
     const key = Cookies.get("key");
     try {
       const { data: { response } } = await api.get("/teams" ,{
@@ -14,6 +14,27 @@ class Teams {
         params: {
           league,
           season
+        }
+      });
+
+      return response
+    } catch (error) {
+      return false
+    }
+  }
+
+  async getStaticsTeams({league, season, team}: {league: string, season: string, team: number}): Promise<IStatiscsRequest | false> {
+    const key = Cookies.get("key");
+    try {
+      const { data: { response } } = await api.get("/teams" ,{
+        headers: {
+          "x-rapidapi-host": "v3.football.api-sports .io",
+          "x-rapidapi-key": key
+        },
+        params: {
+          league,
+          season,
+          team
         }
       });
 
--- src/service/Teams/teams.structure.ts
+++ src/service/Teams/teams.structure.ts
@@ -7,6 +7,42 @@ export interface ITeam {
   logo: string;
 }
 
+export interface ILineup {
+  formation: string;
+  played: number;
+}
+
 export interface ITeamsRequest {
   team: ITeam;
+}
+
+export interface IStaticsGame {
+  home: number;
+  away: number;
+  total: number;
+}
+
+export interface IStatiscsRequest {
+  team: {
+    id: number;
+    name: string;
+    logo: string;
+  },
+  fixtures: {
+    played: IStaticsGame;
+    wins: IStaticsGame;
+    loses: IStaticsGame;
+    draws: IStaticsGame;
+  },
+  goals: {
+    for: {
+      total: IStaticsGame;
+      average: IStaticsGame;
+    },
+    against: {
+      total: IStaticsGame;
+      average: IStaticsGame;  
+    }
+  },
+  lineups: ILineup[]
 }
